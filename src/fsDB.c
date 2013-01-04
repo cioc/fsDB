65e1b8665e71b4d3d65992538921dbe60b115ffe
Charles Cary <cclego@gmail.com>
2013-01-04 01:57:43 +0000

    outline added, will finish tommorrow

--- src/fsDB.c
+++ src/fsDB.c
@@ -1,9 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "net_support.h"
+#include "producer_consumer.h"
+
+producer_consumer prod_cons;
+int server_socket;
+
+void *
+socket_producer(void *data)
+{
+  //TODO - IMPLEMENT network server
+}
+
+void *
+request_handler(void *data)
+{
+  //TODO - GET DATA FROM FILE AND SEND DOWN LINE
+}
+
 
 int 
 main(int argc, char **args) 
 {
-  exit(0);
+  //TODO - GET NUMBER OF THREADS TO RUN, PORT, SIZE OF ITEM, AND MAX
+  //TODO - SETUP PRODUCER_CONSUMER AND START SERVER
 }
 
